4b093c7931197f87a8b19e5ddc4eedff141af5fb
Shukri Adams <shukri.adams@gmail.com>
2023-12-20 13:37:47 +0100

    better server connection quality visualizing

---
+++ src/Converters/ConnectionQualityConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace TetrifactClient
+{
+    public static class ConnectionQualityConverter
+    {
+        public static readonly IValueConverter ToNamedColor = new FuncValueConverter<object?, IBrush>((object arg) => {
+
+            ConnectionQuality quality = (ConnectionQuality)arg;
+            return quality == ConnectionQuality.Broken ? Brush.Parse("Red") :
+                quality == ConnectionQuality.Good ? Brush.Parse("Green") :
+                quality == ConnectionQuality.Degraded ? Brush.Parse("Orange") :
+                Brush.Parse("White");
+        });
+
+        public static readonly IValueConverter ToTooltipString = new FuncValueConverter<object?, string>((object arg) =>
+        {
+            ConnectionQuality quality = (ConnectionQuality)arg;
+            return quality == ConnectionQuality.Broken ? "Server unreachable" :
+                quality == ConnectionQuality.Good  ? "Server connection fast" :
+                quality == ConnectionQuality.Degraded ? "Server connection poor" :
+                "Server connection calculating, please wait...";
+        });
+    }
+}
